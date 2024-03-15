7925ed3c3013b31ccceb4d2cb54ef1217a89d2e3
yanfer <yanfer.daniel@gmail.com>
2024-03-14 18:46:37 -0600

    first commit

---
+++ src/components/Testimonials/Testimonials.data.ts
@@ -0,0 +1,44 @@
+export const dataTestimonials = [
+  {
+    id: 1,
+    name: 'Juan Recio',
+    work: 'Founder',
+    testimonial: 'Excelente aplicación, gracias Yanfer por hacerla.',
+    image: '/assets/profile1.png',
+  },
+  {
+    id: 2,
+    name: 'Laura García',
+    work: 'Founder',
+    testimonial: 'Excelente aplicación, gracias Yanfer por hacerla.',
+    image: '/assets/profile2.png',
+  },
+  {
+    id: 3,
+    name: 'Lucas Martínez',
+    work: 'Founder',
+    testimonial: 'Excelente aplicación, gracias Yanfer por hacerla.',
+    image: '/assets/profile3.png',
+  },
+  {
+    id: 4,
+    name: 'Vicente Calvo',
+    work: 'Founder',
+    testimonial: 'Excelente aplicación, gracias Yanfer por hacerla.',
+    image: '/assets/profile4.png',
+  },
+  {
+    id: 5,
+    name: 'Patricia García',
+    work: 'Founder',
+    testimonial: 'Excelente aplicación, gracias Yanfer por hacerla.',
+    image: '/assets/profile1.png',
+  },
+  {
+    id: 6,
+    name: 'Raúl Vidal',
+    work: 'Founder',
+    testimonial: 'Excelente aplicación, gracias Yanfer por hacerla.',
+    image: '/assets/profile3.png',
+  },
+];
